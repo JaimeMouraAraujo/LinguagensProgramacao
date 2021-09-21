db885a151b0267d183384ecf65db65a58a341355
Jaime Araújo <jaimemoura@sou.faccat.br>
2021-09-20 21:36:31 -0300

    Correção e chamada dos métodos

--- src/aula_4/Aluno.java
+++ src/aula_4/Aluno.java
@@ -1,4 +1,7 @@
 package aula_4;
+
+import java.util.Scanner;
+
 public class Aluno extends Pessoa {
 	private String nome;
 	private int idade;
@@ -9,8 +12,8 @@ public class Aluno extends Pessoa {
 		this.idade = idade;
 		this.isProgramador = isProgramador;
 
-		this.dizerAlgo("Olá, me chamo " + this.nome + ", tenho " + this.idade + " e "
-				+ (this.isProgramador ? "" : "não") + "sou programador.");
+		this.dizerAlgo("Olá, me chamo " + this.nome + ", tenho " + this.idade + " e"
+				+ (this.isProgramador ? "" : " não") + " sou programador.");
 	}
 
 	@Override
@@ -41,5 +44,30 @@ public class Aluno extends Pessoa {
 	@Override
 	float calcularRestoDeDivisao(float v1, float v2) {
 		return v1 % v2;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Informe o nome do aluno: ");
+		String nome = scan.nextLine();
+
+		System.out.print("Informe a idade do aluno: ");
+		int idade = scan.nextInt();
+		scan.nextLine(); // limpa o buffer de entrada
+
+		System.out.print("Informe se o aluno é ou não um programador ([S]im | [N]ão): ");
+		String isProg = scan.nextLine();
+		scan.close();
+
+		Aluno aluno = new Aluno(nome, idade, isProg.charAt(0) == 'S');
+
+		aluno.dizerAlgo("Boa noite");
+
+		aluno.dizerAlgo("multiplicar: " + aluno.multiplicar(2, 3));
+		aluno.dizerAlgo("somar: " + aluno.somar(2, 3));
+		aluno.dizerAlgo("subtrair: " + aluno.subtrair(2, 3));
+		aluno.dizerAlgo("dividir: " + aluno.dividir(2, 3));
+		aluno.dizerAlgo("calcularRestoDeDivisao: " + aluno.calcularRestoDeDivisao(2, 3));
 	}
 }
